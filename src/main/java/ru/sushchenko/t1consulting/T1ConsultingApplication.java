10b70a2d1e64bceb01c057c86d1323f4ac4b917e
lordphiluren <artoymsushchenko@gmail.com>
2024-02-13 12:51:56 +0300

    last commit

--- src/main/java/ru/sushchenko/t1consulting/T1ConsultingApplication.java
+++ src/main/java/ru/sushchenko/t1consulting/T1ConsultingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class T1ConsultingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(T1ConsultingApplication.class, args);
     }
--- src/main/java/ru/sushchenko/t1consulting/config/SwaggerConfig.java
+++ src/main/java/ru/sushchenko/t1consulting/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package ru.sushchenko.t1consulting.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
     info = @Info(
